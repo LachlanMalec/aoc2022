483ed2e97ccd066860f1e37e39158bad46d257dc
Lachlan Malec <lachlan@lachlanmalec.dev>
2022-12-03 20:33:36 +1000

    feat(day-three): solved part 2

--- day-three/src/main.rs
+++ day-three/src/main.rs
@@ -5,11 +5,16 @@ fn main() {
 
     let rucksacks = parse_challenge_data(challenge_data);
 
-    let part_one = part_one(rucksacks);
+    let part_one = part_one(rucksacks.clone());
 
     println!("Part one: {}", part_one);
+
+    let part_two = part_two(rucksacks.clone());
+
+    println!("Part two: {}", part_two);
 }
 
+#[derive(Clone)]
 struct Rucksack {
     compartment_one: Vec<char>,
     compartment_two: Vec<char>,
@@ -23,6 +28,12 @@ impl Rucksack {
             }
         }
         None
+    }
+
+    fn all_items(&self) -> Vec<char> {
+        let mut items = self.compartment_one.clone();
+        items.extend(self.compartment_two.clone());
+        items
     }
 }
 
@@ -49,12 +60,35 @@ fn get_item_priority(item: char) -> u8 {
     }
 }
 
+fn determine_group_badge(group_rucksacks: [Rucksack; 3]) -> char {
+    group_rucksacks[0].all_items().iter()
+        .filter(|item| group_rucksacks[1].all_items().contains(item))
+        .filter(|item| group_rucksacks[2].all_items().contains(item))
+        .next()
+        .map(|item| *item)
+        .unwrap()
+}
+
 fn part_one(rucksacks: Vec<Rucksack>) -> i32 {
     let mut total = 0;
     for rucksack in rucksacks {
         if let Some(duplicated_item) = rucksack.duplicated_item() {
             total += get_item_priority(duplicated_item) as i32;
         }
+    }
+    total
+}
+
+fn part_two(rucksacks: Vec<Rucksack>) -> i32 {
+    let mut total = 0;
+    for rucksack in rucksacks.chunks(3) {
+        let group_rucksacks = [
+            rucksack[0].clone(),
+            rucksack[1].clone(),
+            rucksack[2].clone(),
+        ];
+        let group_badge = determine_group_badge(group_rucksacks);
+        total += get_item_priority(group_badge) as i32;
     }
     total
 }
