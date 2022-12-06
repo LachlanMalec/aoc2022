ab47c84d3dee6eba6a7e91fdf6ced7f4ebd7375f
Lachlan Malec <lachlan@lachlanmalec.dev>
2022-12-06 16:56:10 +1000

    refactor(day-six): reduced repeated code fragments

--- 2022/day-six/src/main.rs
+++ 2022/day-six/src/main.rs
@@ -3,18 +3,27 @@ use std::fs;
 fn main() {
     let datastream_buffer = fs::read("data").expect("Unable to read file");
 
-    println!("Part 1: {}", identify_start_of_packet_marker_position(&datastream_buffer));
+    println!("Part 1: {}", identify_marker_position(&datastream_buffer, Marker::StartOfPacket));
 
-    println!("Part 2: {}", identify_start_of_message_marker_position(&datastream_buffer));
+    println!("Part 2: {}", identify_marker_position(&datastream_buffer, Marker::StartOfMessage));
 }
 
-fn identify_start_of_packet_marker_position(datastream_buffer: &[u8]) -> usize {
+enum Marker {
+    StartOfPacket,
+    StartOfMessage,
+}
+
+fn identify_marker_position(datastream_buffer: &[u8], marker: Marker) -> usize {
+    let marker_length = match marker {
+        Marker::StartOfPacket => 4,
+        Marker::StartOfMessage => 14,
+    };
     let mut marker_position: Option<usize> = None;
     for (i, _) in datastream_buffer.iter().enumerate() {
         let mut unique_chars = 0;
-        for j in 0..4 {
+        for j in 0..marker_length {
             let mut unique = true;
-            for k in 0..4 {
+            for k in 0..marker_length {
                 if j != k && datastream_buffer[i + j] == datastream_buffer[i + k] {
                     unique = false;
                     break;
@@ -24,32 +33,8 @@ fn identify_start_of_packet_marker_position(datastream_buffer: &[u8]) -> usize {
                 unique_chars += 1;
             }
         }
-        if unique_chars == 4 {
-            marker_position = Some(i + 4);
-            break;
-        }
-    }
-    marker_position.expect("Unable to find marker")
-}
-
-fn identify_start_of_message_marker_position(datastream_buffer: &[u8]) -> usize {
-    let mut marker_position: Option<usize> = None;
-    for (i, _) in datastream_buffer.iter().enumerate() {
-        let mut unique_chars = 0;
-        for j in 0..14 {
-            let mut unique = true;
-            for k in 0..14 {
-                if j != k && datastream_buffer[i + j] == datastream_buffer[i + k] {
-                    unique = false;
-                    break;
-                }
-            }
-            if unique {
-                unique_chars += 1;
-            }
-        }
-        if unique_chars == 14 {
-            marker_position = Some(i + 14);
+        if unique_chars == marker_length {
+            marker_position = Some(i + marker_length);
             break;
         }
     }
