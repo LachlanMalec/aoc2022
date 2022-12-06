3918de3fb40ab805fa02d23484362dee1e7fb656
Lachlan Malec <lachlan@lachlanmalec.dev>
2022-12-06 16:52:04 +1000

    feat(day-six): solved part two

--- 2022/day-six/src/main.rs
+++ 2022/day-six/src/main.rs
@@ -3,7 +3,9 @@ use std::fs;
 fn main() {
     let datastream_buffer = fs::read("data").expect("Unable to read file");
 
-    println!("Marker position: {}", identify_start_of_packet_marker_position(&datastream_buffer));
+    println!("Part 1: {}", identify_start_of_packet_marker_position(&datastream_buffer));
+
+    println!("Part 2: {}", identify_start_of_message_marker_position(&datastream_buffer));
 }
 
 fn identify_start_of_packet_marker_position(datastream_buffer: &[u8]) -> usize {
@@ -24,6 +26,30 @@ fn identify_start_of_packet_marker_position(datastream_buffer: &[u8]) -> usize {
         }
         if unique_chars == 4 {
             marker_position = Some(i + 4);
+            break;
+        }
+    }
+    marker_position.expect("Unable to find marker")
+}
+
+fn identify_start_of_message_marker_position(datastream_buffer: &[u8]) -> usize {
+    let mut marker_position: Option<usize> = None;
+    for (i, _) in datastream_buffer.iter().enumerate() {
+        let mut unique_chars = 0;
+        for j in 0..14 {
+            let mut unique = true;
+            for k in 0..14 {
+                if j != k && datastream_buffer[i + j] == datastream_buffer[i + k] {
+                    unique = false;
+                    break;
+                }
+            }
+            if unique {
+                unique_chars += 1;
+            }
+        }
+        if unique_chars == 14 {
+            marker_position = Some(i + 14);
             break;
         }
     }
